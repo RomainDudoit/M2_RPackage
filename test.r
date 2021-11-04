793f50aa9451824d040383080720e8e2d4c9b418
Romain Dudoit <ro.dudoit@gmail.com>
2021-11-04 01:04:38 +0100

    test

--- test.r
+++ test.r
@@ -1,7 +1,6 @@
-# Régression logistique et descente de gradient
+library(readxl)
 
-
-# Fonction sigmoïde (comprise entre 0 et 1)
+# sigmoid function 
 sigmoid <- function(x) {
     return(1 / (1 + exp(-x)))
 }
@@ -16,35 +15,54 @@ probability <- function(x, theta) {
 }
 
 
-# Fonction de coût
-cost_function <- function(X, y, theta) {
-    m <- nrow(y) # nombre d'observations
-    g <- probability(X, theta)
+# cost function
+cost_function <- function(x, y, theta) {
+    m <- nrow(y)
+    g <- probability(x, theta)
     j <- (1 / m) * sum((-y * log(g)) - ((1 - y) * log(1 - g)))
     return(j)
 }
 
-# Calcul du gradient
+# Gradient
 gradient <- function(x, y, theta) {
     m <- nrow(y)
     return((1 / m) * x_dot_theta(t(x), probability(x, theta) - y))
 }
 
-
-# Optimisation de theta
-optimisation <- function(X, y, theta, learning_rate) {
-    gradient <- gradient(X, y, theta)
+# Gradient descent 
+optimisation <- function(x, y, theta, learning_rate) {
+    gradient <- gradient(x, y, theta)
     theta <- theta - learning_rate * gradient
     return(theta)
 }
 
+# batch descent gradient, modifier selon le mode stochastique ou mini batch
+logistic_regression <- function(x, y, learning_rate = 0.1, n_iter = 100) {
+    # Initialisation des paramètres
+    theta <- as.matrix(rnorm(n = dim(x)[2], mean = 0, sd = 1))
 
-fit <- function() {
-
+    # Optimisation des paramètres pour un certain nombre d'itérations
+    for (i in range(n_iter)) {
+        theta <- optimisation(x, y, theta, learning_rate)
+        print(theta)
+    }
+    return(theta)
 }
 
-logistic_regression <- function(X, y, learning_rate = 0.1, n_iter = 100) {
+data <- read_excel("M2_RPackage/breast.xlsx")
+
+# Régression logistique et descente de gradient
+
+y <- data[, ncol(data)]
+y <- ifelse(y == "malignant", 1, 0)
 
 
+x <- data[, 1:(ncol(data) - 1)]
 
-}
+theta <- as.matrix(rnorm(n = dim(x)[2], mean = 0, sd = 1))
+
+
+x <- as.matrix(data.frame(rep(1, length(y)), x))
+n <- dim(x)
+
+best_theta <- logistic_regression(x, y, learning_rate = 0.1, n_iter = 500)
