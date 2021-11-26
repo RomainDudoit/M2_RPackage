5150bdc11809af030df4c3623411bededb5e54f7
Romain Dudoit <ro.dudoit@gmail.com>
2021-11-26 03:18:41 +0100

    upd

--- rlgd/R/cost_function.R
+++ rlgd/R/cost_function.R
@@ -8,10 +8,11 @@
 #' @export
 #'
 #' @examples
-#' x <- as.matrix(breast_cancer[,1:4])
-#' y <- as.matrix(ifelse(breast_cancer$classe =="malignant",1,0))
+#' x <- as.matrix(breast_cancer[1:10,1:4])
+#' y <- breast_cancer[1:10,"classe"]
+#' y <- as.matrix(ifelse(y$classe =="malignant",1,0))
 #' theta <- as.matrix(rnorm(n = dim(x)[2], mean = 0, sd = 1))
-#' cost_function(x,y,theta)
+#' cost_function(x,y,as.matrix(rnorm(n = dim(x)[2], mean = 0, sd = 1)))
 cost_function <- function(x, y, theta) {
   m <- nrow(y)
   g <- probability(x, theta)
--- rlgd/R/x_dot_theta.R
+++ rlgd/R/x_dot_theta.R
@@ -7,7 +7,9 @@
 #' @export
 #'
 #' @examples
-#' x <- as.matrix(breast_cancer[,1:4])
+#' x <- as.matrix(breast_cancer[1:10,1:4])
+#' y <- breast_cancer[1:10,"classe"]
+#' y <- as.matrix(ifelse(y$classe =="malignant",1,0))
 #' theta <- as.matrix(rnorm(n = dim(x)[2], mean = 0, sd = 1))
 #' x_dot_theta(x,theta)
 x_dot_theta <- function(x, theta) {
